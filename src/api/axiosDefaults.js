959d64866c2615c3722565dd77d2263924d811e2
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-01-07 12:28:40 +0100

    feat: create a signup form

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,8 @@
 import axios from "axios";
-
+// Set the base URL for all Axios requests
 axios.defaults.baseURL =
   "https://drf-api-app-gaysha-03eb690a65c6.herokuapp.com/";
+// Set the default Content-Type header for POST requests to "multipart/form-data"
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+// Allow sending credentials (like cookies) with cross-origin requests
 axios.defaults.withCredentials = true;
--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,20 +1,57 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Image,
+  Col,
+  Row,
+  Container,
+  Alert,
+} from "react-bootstrap";
+import axios from "axios";
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = signUpData;
+  const [errors, setErrors] = useState({});
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      // The square brackets [] denote a computed property name. Inside the brackets is an expression (event.target.name) that will be evaluated to determine the property name dynamically.
+      // event.target.name refers to the name attribute of the input element triggering the  handleChange function. This is typically used in forms to identify which form field is being updated.
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration/", signUpData);
+      history.push("/sinin");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
 
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">username</Form.Label>
               <Form.Control
@@ -22,8 +59,16 @@ const SignUpForm = () => {
                 type="text"
                 placeholder="username"
                 name="username"
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
+
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="password1">
               <Form.Label className="d-none">Password</Form.Label>
@@ -32,6 +77,8 @@ const SignUpForm = () => {
                 type="password"
                 placeholder="Password"
                 name="password1"
+                value={password1}
+                onChange={handleChange}
               />
             </Form.Group>
 
@@ -42,6 +89,8 @@ const SignUpForm = () => {
                 type="password"
                 placeholder="Confirm password"
                 name="password2"
+                value={password2}
+                onChange={handleChange}
               />
             </Form.Group>
 
