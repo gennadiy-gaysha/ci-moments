3247bb9440b283a2602a7ebe8f20eeb71d84c253
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-01-09 16:04:12 +0100

    fix: fix axiosDefaults

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -8,4 +8,4 @@ axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
-export const awiosRes = axios.create();
+export const axiosRes = axios.create();
