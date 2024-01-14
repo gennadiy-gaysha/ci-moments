f3920be2e57c07c0f10b14d95ad7e64616c0a6ef
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-01-14 13:05:15 +0100

    feat: add sinner for loading posts list

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -5,11 +5,15 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
+import Post from "./Post";
+import Asset from "../../components/Asset";
+
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
-import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
-import Post from "./Post";
+
+import NoResults from "../../assets/no-results.png";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -19,43 +23,38 @@ function PostsPage({ message, filter = "" }) {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const { data } = await axiosReq(`/posts/?${filter}`);
+        const { data } = await axiosReq.get(`/posts/?${filter}`);
         setPosts(data);
         setHasLoaded(true);
       } catch (err) {
         console.log(err);
       }
     };
+
     setHasLoaded(false);
     fetchPosts();
-    // Array of dependencies - The effect will run whenever any of the dependencies change. In this case, the effect will run if filter or pathname changes.
   }, [filter, pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
-        <i className={`fas fa-search ${styles.SearchIcon}`} />
-        <Form
-          className={styles.SearchBar}
-          onSubmit={(event) => event.preventDefault()}
-        >
-          <Form.Control
-            type="text"
-            className="mr-sm-2"
-            placeholder="Search posts"
-          />
-        </Form>
         {hasLoaded ? (
           <>
-            {posts.results.length
-              ? posts.results.map((post) => (
-                  <Post key={post.id} setPosts={setPosts} />
-                ))
-              : console.log("show no results asset")}
+            {posts.results.length ? (
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
           </>
         ) : (
-          console.log("show loading spinner")
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
