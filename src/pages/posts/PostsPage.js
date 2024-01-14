f29db93a868d7beb66a40f1b652a31ff89ba07b4
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-01-14 15:42:24 +0100

    fix: fix InfiniteScroll-2

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -14,8 +14,6 @@ import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.png";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { fetchMoreData } from "../../utils/utils";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -62,15 +60,9 @@ function PostsPage({ message, filter = "" }) {
         {hasLoaded ? (
           <>
             {posts.results.length ? (
-              <InfiniteScroll
-                children={posts.results.map((post) => (
-                  <Post key={post.id} {...post} setPosts={setPosts} />
-                ))}
-                dataLength={posts.results.length}
-                loader={<Asset spinner />}
-                hasMore={!!posts.next}
-                next={() => fetchMoreData(posts, setPosts)}
-              />
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
             ) : (
               <Container className={appStyles.Content}>
                 <Asset src={NoResults} message={message} />
