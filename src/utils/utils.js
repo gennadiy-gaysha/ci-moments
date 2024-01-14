e260dd5e0fbec6514a9edb13c8fd488364f69f5e
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-01-14 15:33:38 +0100

    fix: fix InfiniteScroll

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,15 +1,15 @@
-import axios from "axios";
+import { axiosReq } from "../api/axiosDefaults";
 
 export const fetchMoreData = async (resource, setResource) => {
   try {
-    const { data } = await axios.get(resource.next);
+    const { data } = await axiosReq.get(resource.next);
     setResource((prevResource) => ({
       ...prevResource,
       next: data.next,
-      results: data.results.reduce((acc, curr) => {
-        return acc.some((accResult) => accResult.id === curr.id)
+      results: data.results.reduce((acc, cur) => {
+        return acc.some((accResult) => accResult.id === cur.id)
           ? acc
-          : [...acc.cur];
+          : [...acc, cur];
       }, prevResource.results),
     }));
   } catch (err) {}
